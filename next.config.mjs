1a2999fd8cfef5ce0dc28f6ad4c6730233abcef5
anton <antonosland789@gmail.com>
2024-08-30 22:08:07 -0500

    added "/readingforward" as root path in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // sets to static site
     output: 'export',
+    // root path for pub folder imgs
+    basePath: '/readingforward'
 };
 
 export default nextConfig;
