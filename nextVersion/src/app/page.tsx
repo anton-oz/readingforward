15ac01ea8cded8996c4908fa435ff768adbf614e
anton <antonosland789@gmail.com>
2024-09-04 21:31:34 -0500

    syncing react with next

--- nextVersion/src/app/page.tsx
+++ nextVersion/src/app/page.tsx
@@ -1,19 +1,16 @@
-import {FC} from 'react'
+import { FC } from "react";
 
 import { ReadingForwardLogo } from "@/components/readingForwardLogo";
 
 import Book from "@/components/Book";
 
-
 export default function Home() {
-  
   return (
-      <section className="h-full w-full grid grid-rows-3 text-center">
-        <div className="flex flex-col items-center justify-center">
-          <Book />
-        </div>
-        <p className="font-bold text-xl">Turning Pages, Igniting Minds</p>
-      </section>
+    <section className="h-full w-full grid grid-rows-3 text-center">
+      <div className="flex flex-col items-center justify-center">
+        {/* <Book /> */}
+      </div>
+      <p className="font-bold text-xl">Turning Pages, Igniting Minds</p>
+    </section>
   );
 }
-
---
+++ reactVersion/src/components/Nav.tsx
@@ -0,0 +1,9 @@
+function Nav() {
+  return (
+    <nav>
+      <p>navbar</p>
+    </nav>
+  );
+}
+
+export default Nav;
